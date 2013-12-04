9c6e83517389efc0c5d98066a05d7dc7029847fc
tminglei <tmlneu@gmail.com>
2013-12-04 08:47:00 +0800

    enhance Converters/ConverterUtil

--- core/src/main/scala/com/github/tminglei/slickpg/utils/Converters.scala
+++ core/src/main/scala/com/github/tminglei/slickpg/utils/Converters.scala
@@ -72,14 +72,14 @@ object ConverterUtil {
         } else (tpe, false)
       )
 
-      ///
+      //--
       def apply(elem: Element): T = {
         val classMirror = runtimeMirror.reflectClass(thisType.typeSymbol.asClass)
         val ctorMirror = classMirror.reflectConstructor(constructor)
 
         val args = elem.asInstanceOf[CompositeE].members.zip(ctorArgInfo).map {
           case (e, (tpe, isOption)) => {
-            val tv = convert(e, tpe)
+            val tv = convertToValue(e, tpe)
             if (isOption) Option(tv) else tv
           }
         }
@@ -100,9 +100,10 @@ object ConverterUtil {
           (realType, true)
         } else (thisType, false)
 
+      //--
       def apply(elem: Element): List[T] = {
         elem.asInstanceOf[ArrayE].elements.map { e =>
-          val tv = convert(e, realType)
+          val tv = convertToValue(e, realType)
           if (isOption) Option(tv) else tv
         } map (_.asInstanceOf[T])
       }
@@ -113,35 +114,72 @@ object ConverterUtil {
   private val strType  = ru.typeOf[String]
   private val elemType = ru.typeOf[Element]
 
-  private def convert(e: Element, toType: ru.Type): Any = e match {
+  private def convertToValue(e: Element, toType: ru.Type): Any = e match {
     case ValueE(v)  => Converters.internalGet(strType, toType).get.asInstanceOf[Converter[String,_]](v)
     case _: CompositeE  => Converters.internalGet(elemType, toType).get.asInstanceOf[Converter[Element,_]](e)
     case ArrayE(el) => {
       val eType = toType.asInstanceOf[ru.TypeRef].args(0)
-      el.map(e => convert(e, eType))
+      el.map(e => convertToValue(e, eType))
+    }
+  }
+
+  private def convertToElement(v: Any, fromType: ru.Type): Element = {
+    (v, fromType) match {
+      case (Some(realVal), _) => {
+        val realType = fromType.asInstanceOf[ru.TypeRef].args(0)
+        convertToElement(realVal, realType)
+      }
+      case (vList: List[_], _) => {
+        val elemType = fromType.asInstanceOf[ru.TypeRef].args(0)
+        ArrayE(vList.map(convertToElement(_, elemType)))
+      }
+      case (_, _) => {
+        Converters.internalGet(fromType, elemType)
+          .map(_.asInstanceOf[Converter[Any, Element]](v))
+          .getOrElse(if (v == null) null else ValueE(v.toString) )
+      }
     }
   }
 
   ////
-  def mkConvToValueTree[T](implicit ev: ru.TypeTag[T], ev1: ClassTag[T]): Converter[T, List[Any]] = {
+  def mkConvToElement[T](implicit ev: ru.TypeTag[T], ev1: ClassTag[T]): Converter[T, Element] = {
     
-    new Converter[T, List[Any]] {
-      val thisType = ru.typeOf[T]
-      
-      val constructor = thisType.declaration(ru.nme.CONSTRUCTOR).asMethod
-      val ctorFields  = constructor.paramss.head.map(_.name).map(thisType.declaration(_).asTerm)
+    new Converter[T, Element] {
+      private val thisType = ru.typeOf[T]
+
+      private val constructor = thisType.declaration(ru.nme.CONSTRUCTOR).asMethod
+      private val ctorFieldInfo = constructor.paramss.head.map(t => {
+        val field = thisType.declaration(t.name).asTerm
+        val tpe  = t.typeSignature
+        (field, tpe)
+      })
       
       //--
-      def apply(v: T): List[Any] = {
+      def apply(v: T): Element = {
         val instanceMirror = runtimeMirror.reflect(v)
-        ctorFields.map( field => {
-          val fv = instanceMirror.reflectField(field).get
-          val rv = if (fv.isInstanceOf[Option[_]]) fv.asInstanceOf[Option[_]].getOrElse(null) else fv
-//          rv match {
-//            case lv: Seq[_] => val conv = internalGet
-//          }
-          rv
-        })
+        CompositeE(
+          ctorFieldInfo.map {
+            case (field, tpe) => {
+              val fv = instanceMirror.reflectField(field).get
+              convertToElement(fv, tpe)
+            }
+          }
+        )
+      }
+    }
+  }
+
+  def mkArrayConvToElement[T](implicit ev: ru.TypeTag[T], ev1: ClassTag[T]): Converter[List[T], Element] = {
+
+    new Converter[List[T], Element] {
+      private val thisType = ru.typeOf[T]
+
+      def apply(vList: List[T]): Element = {
+        ArrayE(
+          vList.map { v =>
+            convertToElement(v, thisType)
+          }
+        )
       }
     }
   }
@@ -158,7 +196,7 @@ object ConverterUtil {
     val conv = mkConvFromElement[T]
     println(conv(CompositeE(List(ValueE("111"), ValueE("test"), ValueE("test desc"), ValueE(null)))))
     
-    val conv1 = mkConvToValueTree[T]
+    val conv1 = mkConvToElement[T]
     println(conv1(T(112, "test", "test 2")))
     
     println("ok")
@@ -167,7 +205,8 @@ object ConverterUtil {
     val convt1 = mkConvFromElement[T1]
     println(convt1(CompositeE(List(ValueE("115"), CompositeE(List(ValueE("111"), ValueE("test"), ValueE("test dd"), ValueE("hi"))), ArrayE(List(ValueE("157")))))))
 
-    val convt11 = mkConvToValueTree[T1]
+    Converters.register(conv1)
+    val convt11 = mkConvToElement[T1]
     println(convt11(T1(116, T(111, "test", "test 3"), List(123,135))))
 
     println("ok")
@@ -175,5 +214,11 @@ object ConverterUtil {
     Converters.register(convt1)
     val convat = mkArrayConvFromElement[T1]
     println(convat(ArrayE(List(CompositeE(List(ValueE("115"), CompositeE(List(ValueE("111"), ValueE("test"), ValueE("test dd"), ValueE("hi"))), ArrayE(List(ValueE("157")))))))))
+
+    Converters.register(convt11)
+    val convat1 = mkArrayConvToElement[T1]
+    println(convat1(List(T1(115,T(111,"test","test dd",Some("hi")),List(157)))))
+
+    println("ok")
   }
 }
